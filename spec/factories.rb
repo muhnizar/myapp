c78867fa365526d756a79490d24d77f5466bdd46
muhnizar <mooh.nizar@yahoo.com>
2014-03-28 16:38:05 +0700

    add gravater

---
+++ spec/factories.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name     "Nizar Muhammad"
+    email    "mooh.nizar@gmail.com"
+    password "nizarkeren"
+    password_confirmation "nizarkeren"
+  end
+end
