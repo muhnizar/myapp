2bc64fad5e5bff868d00524b8d9fab594d750b74
muhnizar <mooh.nizar@yahoo.com>
2014-07-21 20:09:05 +0700

    add user microposts

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,8 +17,13 @@ class User < ActiveRecord::Base
     Digest::SHA1.hexdigest(token.to_s)
   end
 
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
   private
 
+  
     def create_remember_token
       self.remember_token = User.hash(User.new_remember_token)
     end
